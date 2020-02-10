ee57a218315efc960b22f25b9c5d9b94aad5a376
Nicolas Martin <nicolas27martin@gmail.com>
2020-02-10 21:47:49 +0100

    add lastest changes

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,50 +1,47 @@
 <template>
-  <div v-show="showT">
-    <h1>Contact page</h1>
-    <div class="result"></div>
-    <input type="text" class="inputT" />
+    <div>
+        <input type="text" class="input3">
+        <div class="result"></div>
+        <form @submit="checkForm" action="/" method="POST">
+            <h1>{{input1}} {{input2}}</h1>
+            <input type="text" class="input1" v-model="input1">
+            <input type="text" class="input2" v-model="input2">
 
-    <form @submit="checkForm" action="/" method="post">
-      <h2>result: {{input1}} {{input2}}</h2>
-      <input type="text" class="input1" v-model="input1" placeholder="message 1" />
-      <input type="text" class="input2" v-model="input2" placeholder="message 2" />
-
-      <button>submit</button>
-    </form>
-  </div>
+            <button>submit</button>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-  layout: "contactlayout",
-  data() {
-    return {
-      showT: false,
-      input1: null,
-      input2: null
-    };
-  },
-  mounted() {
-    window.onkeyup = keyup;
-    let inputTV;
+    layout: "contactlayout",
+    data() {
+        return {
+            input1: null,
+            input2: null
+        }
+    },
+    methods: {
+        checkForm(e) {
+            if(this.input1 && this.input2) {
+                alert('submit');
+            }
+            e.preventDefault();
+        }
+    },
+    mounted() {
+        window.onkeyup = keyup;
+        let textV;
 
-    function keyup(e) {
-      if(e.keyCode >= 65 && e.keyCode <= 90) {
-        inputTV = e.target.value;
-        document.querySelector('.result').innerHTML = inputTV; 
-      }
-    };
+        function keyup(e) {
+            if(e.keyCode >= 65 && e.keyCode <= 90) {
+                textV = e.target.value;
+                console.warn(textV);
+                document.querySelector('.result').innerHTML = textV;
+            }
+        }
 
-    keyup(document.querySelector('.inputT'));
-  },
-  methods: {
-    checkForm(e) {
-      if (this.input1 && this.input2) {
-        alert("submit");
-        return true;
-      }
-      e.preventDefault();
+        keyup(document.querySelector('.input3'));
     }
-  }
-};
+}
 </script>
